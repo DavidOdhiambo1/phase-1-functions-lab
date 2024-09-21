45624f38a69e5b22a300cca1c77f18eb0fc4e71d
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-09-21 11:42:17 +0300

    calculates dist in blocks/feets/price

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLoc){
+    if(pickUpLoc>42){
+        return pickUpLoc - 42
+    } else {
+        return 42 - pickUpLoc
+    }
+}
+//console.log(distanceFromHqInBlocks(50))
+
+function distanceFromHqInFeet(pickUpLoc){
+    return distanceFromHqInBlocks(pickUpLoc)*264
+}
+//console.log(distanceFromHqInFeet(43))
+
+function distanceTravelledInFeet(startBlock, endBlock){
+    if (endBlock>startBlock){
+        return (endBlock - startBlock)*264
+    }else {
+        return (startBlock-endBlock)*264
+    } 
+}
+console.log(distanceTravelledInFeet(34, 24))
+
+function calculatesFarePrice(startBlock, endBlock){
+    if ((distanceTravelledInFeet(startBlock, endBlock)) <= 400){
+        return 0
+    } else if((distanceTravelledInFeet(startBlock, endBlock)) <= 2000){
+        return ((distanceTravelledInFeet(startBlock, endBlock)) -400)*0.02
+    } else if((distanceTravelledInFeet(startBlock, endBlock)) >2000 && (distanceTravelledInFeet(startBlock, endBlock)) <2500 ){
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
+console.log (calculatesFarePrice(34, 24))
